08527ea1c4e6ed5e13496f78dbac570e3abc3664
Nguyen Huy Binh <nguyenhuybinh270709@gmail.com>
2026-08-21 15:41:11 +0700

    chores(tin-tuc): add table styling for tin-tuc Content.tsx

--- components/tin-tuc/[slug]/Content.tsx
+++ components/tin-tuc/[slug]/Content.tsx
@@ -18,7 +18,7 @@ export function Content({ post }: { post: Blog }) {
               />
             </div>
             <div
-              className="text-white/80 text-base lg:text-[17px] leading-snug whitespace-pre-wrap
+              className="text-white/80 text-base lg:text-[17px] leading-snug whitespace-pre-wrap text-justify
                 [&_h2]:text-white [&_h2]:text-2xl [&_h2]:font-semibold
                 [&_strong]:text-white
                 [&_b]:text-white/90
@@ -29,7 +29,11 @@ export function Content({ post }: { post: Blog }) {
                 [&_img]:w-full
                 [&_img]:h-auto
                 [&_img]:rounded-2xl
-                [&_img]:object-cover"
+                [&_img]:object-cover
+                [&_table]:w-full [&_table]:my-6 [&_table]:border-collapse [&_table]:border [&_table]:border-white/20 [&_table]:text-left [&_table]:overflow-x-auto [&_table]:block lg:[&_table]:table
+                [&_th]:border [&_th]:border-white/20 [&_th]:bg-white/10 [&_th]:p-3 [&_th]:lg:p-4 [&_th]:font-semibold [&_th]:text-white
+                [&_td]:border [&_td]:border-white/20 [&_td]:p-3 [&_td]:lg:p-4 [&_td]:text-white/80
+                [&_tr]:even:bg-white/2"
               dangerouslySetInnerHTML={{
                 __html: post.acf?.content || "",
               }}
